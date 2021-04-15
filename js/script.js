95c19c8f72cf8b6f10d367009f6dbfdd3d908fad
paolotrimini <paolo.trimini@gmail.com>
2021-04-15 16:28:47 +0200

    generatore email

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 // Attraverso l'apposita API di Boolean generare 10 indirizzi email
 // e stamparli in pagina all'interno di una lista.
+
+
+
+    function emailGenerator() {
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            method: 'GET',
+            success: function(data) {
+                const res = data.response;
+                generateEmail(res);
+            },
+            error: function() {
+                console.log('error');
+            }
+        });
+    }
+    function generateEmail(value) {
+        const target = $('#box');
+        target.append('<ul><li>' + value + '</li></ul>');
+    }
+
+
+    function init() {
+        emailGenerator();
+
+    }
+    document.addEventListener('DOMContentLoaded', init);
